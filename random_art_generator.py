edad0490e4b02f4edc0b781f6324a246fece5ff3
Muhammad Ahsan <69692532+Muhammad-Ahsan-Rasheed@users.noreply.github.com>
2021-12-11 13:48:06 +0500

    Update random_art_generator.py

--- random_art_generator.py
+++ random_art_generator.py
@@ -1,7 +1,5 @@
-import turtle 
-from random import randint
+import turtle
 import random
-
 turtle.title('Colorful pattern')
 turtle.setup(1270, 950, 0, 0)
 turtle.hideturtle()
@@ -9,14 +7,13 @@ turtle.bgcolor('black')
 turtle.tracer(4, 10)
 turtle.speed(15)
 turtle.pensize(10)
-
 turtle.colormode(255)
 angle = [103, 154, 108, 121, 135, 160, 177, 140, 120, 144, 100, 108, 276, 90, 289]
 s_angle = random.choice(angle)
 color = ['indigo', 'blue', 'green', 'yellow', 'orange', 'red']
 for i in range(5, 800):
-    turtle.pencolor(color[i % len(color)]) 
+    turtle.pencolor(color[i % len(color)])
     turtle.forward(i)
     turtle.right(s_angle)
-
+    turtle.width(i / 10)
 turtle.done()
